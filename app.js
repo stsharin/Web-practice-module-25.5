d59101972205e599d342fe60994adc68ada7eff2
stsharin <stssharin@gmail.com>
2021-02-07 01:50:05 +0600

    added single country details

--- app.js
+++ app.js
@@ -13,11 +13,29 @@ const displayCountries = countries => {
         const countryInfo = `
                 <h3 class="country-name">${country.name}</h3>
                 <p>${country.capital}</p>
+                <button onClick="displayCountryDetail('${country.name}')">Details</button>
             `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     });
 
+}
+
+const displayCountryDetail = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderCountryInfo(data[0])); // array element
+}
+const renderCountryInfo = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+    <h1>${country.name}</h1>
+    <p>Population: ${country.population}</p>
+    <p>Area: ${country.area}</p>
+    <img src="${country.flag}">
+    `
 }
 
 // for (let i = 0; i < countries.length; i++) {
